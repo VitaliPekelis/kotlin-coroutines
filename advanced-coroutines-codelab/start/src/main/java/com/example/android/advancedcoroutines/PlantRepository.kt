c25fd02e958857773a7b3d2c45359c46ba25eba2
vitali <pedalka@gmail.com>
2021-11-02 15:04:35 +0200

    code lab "https://developer.android.com/codelabs/advanced-kotlin-coroutines#5" step 5 and 6.

--- advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -16,6 +16,11 @@
 
 package com.example.android.advancedcoroutines
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
+import com.example.android.advancedcoroutines.util.CacheOnSuccess
+import com.example.android.advancedcoroutines.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
@@ -38,14 +43,36 @@ class PlantRepository private constructor(
      * Fetch a list of [Plant]s from the database.
      * Returns a LiveData-wrapped List of Plants.
      */
-    val plants = plantDao.getPlants()
+    val plants: LiveData<List<Plant>> = /*plantDao.getPlants()*/
+        liveData<List<Plant>> {
+            val plantsLiveData = plantDao.getPlants()
+            val customSortOder = plantsListSortOrderCache.getOrAwait()
+
+            emitSource(plantsLiveData.map {
+                plantList -> plantList.applySort(customSortOder)
+            })
+        }
+
+
+    private var plantsListSortOrderCache =
+        CacheOnSuccess(onErrorFallback = { listOf() }) {
+            plantService.customPlantSortOrder()
+    }
 
     /**
      * Fetch a list of [Plant]s from the database that matches a given [GrowZone].
      * Returns a LiveData-wrapped List of Plants.
      */
-    fun getPlantsWithGrowZone(growZone: GrowZone) =
-        plantDao.getPlantsWithGrowZoneNumber(growZone.number)
+    fun getPlantsWithGrowZone(growZone: GrowZone): LiveData<List<Plant>> =
+        /*plantDao.getPlantsWithGrowZoneNumber(growZone.number)*/
+        liveData<List<Plant>> {
+            val plantsGrowZoneLiveData = plantDao.getPlantsWithGrowZoneNumber(growZone.number)
+            val customSortOrder = plantsListSortOrderCache.getOrAwait()
+
+            emitSource(plantsGrowZoneLiveData.map { plantList ->
+                plantList.applySort(customSortOrder)
+            })
+        }
 
     /**
      * Returns true if we should make a network request.
@@ -89,6 +116,15 @@ class PlantRepository private constructor(
     private suspend fun fetchPlantsForGrowZone(growZone: GrowZone) {
         val plants = plantService.plantsByGrowZone(growZone)
         plantDao.insertAll(plants)
+    }
+
+    private fun List<Plant>.applySort(customSortOrder: List<String>) : List<Plant> {
+        return sortedBy { plant ->
+            val positionForItem = customSortOrder.indexOf(plant.plantId).let { order ->
+                if(order > -1) order else Int.MAX_VALUE
+            }
+            ComparablePair(positionForItem, plant.name)
+        }
     }
 
     companion object {
